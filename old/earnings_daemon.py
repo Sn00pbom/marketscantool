59ad8754e4c0b098a51bb88aa9e0e424f035cd89
Zach Lefin <zachlefin@gmail.com>
2019-11-20 16:42:39 -0500

    moved to 'old' folder

---
+++ old/earnings_daemon.py
@@ -0,0 +1,8 @@
+import time
+import schedule
+import earnings_thresh_rev
+
+print('Loading configuration...')
+#TODO load config
+TRIGGER_TIME = time.time(15, 30, 0, 0)
+print('Starting Earnings Daemon...')
