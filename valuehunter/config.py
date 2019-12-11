d5498239dd59defa550eda04b26e9b47553fb169
Zach Lefin <zachlefin@gmail.com>
2019-12-10 23:17:05 -0500

    Changed auto path compat.

--- valuehunter/config.py
+++ valuehunter/config.py
@@ -1,9 +1,10 @@
-import os
+from pathlib import Path
 
 DEBUG = False
 ALPHAVANTAGE_API_KEY = 'E1268XS0ZC9SPFPD'
 QUANDL_API_KEY = 'RxHHiqNxgdyLiKbvtrky'
-SCAN_FOLDER_PATH = os.environ['USERPROFILE'] + '/Desktop/output/'
-SYMBOL_HISTORY_PATH = 'C:/dataset/amex-nyse-nasdaq-stock-histories/fh_20190420/full_history/'
-ALL_EARNINGS_PATH = 'C:/dataset/us-historical-stock-prices-with-earnings-data/stocks_latest/earnings_latest.csv'
-ALL_PRICES_PATH = 'C:/dataset/us-historical-stock-prices-with-earnings-data/stocks_latest/stock_prices_latest.csv'
+HOME_PATH = str(Path.home())
+SCAN_FOLDER_PATH = HOME_PATH + '/Desktop/output'
+SYMBOL_HISTORY_PATH = HOME_PATH + '/dataset/amex-nyse-nasdaq-stock-histories/fh_20190420/full_history/'
+ALL_EARNINGS_PATH = HOME_PATH + '/dataset/us-historical-stock-prices-with-earnings-data/stocks_latest/earnings_latest.csv'
+ALL_PRICES_PATH = HOME_PATH + '/dataset/us-historical-stock-prices-with-earnings-data/stocks_latest/stock_prices_latest.csv'
