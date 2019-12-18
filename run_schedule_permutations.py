6bcca571fe25b2b4f120bd5b89a9964f1b8b890c
Zach Lefin <zachlefin@gmail.com>
2019-12-17 20:14:29 -0500

    added pyname arg to run perms script

--- run_schedule_permutations.py
+++ run_schedule_permutations.py
@@ -8,7 +8,7 @@ def run(pargs=None):
 
     asc = argschedule.ArgSchedule(r_args.schedulepath)
     for args in asc:
-        command = ['python', r_args.scriptpath] + args
+        command = [r_args.pyname, r_args.scriptpath] + args
         print('Running command:', command)
         subprocess.call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         print('Finished')
@@ -24,6 +24,10 @@ def parse_args(pargs=None):
     parser.add_argument('schedulepath',
                         metavar='SCHEDULEPATH',
                         help='Path to a schedule.json with schedule formatting')
+
+    parser.add_argument('--pyname', '-pn', default='python',
+                        metavar='PYTHONNAME',
+                        help='Name of python executable or path (default: python)')
 
     return parser.parse_args(pargs)
 
