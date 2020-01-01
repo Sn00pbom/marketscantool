fb381f67adeefaf2fc6ea32f84fd810876aef0a0
Zach Lefin <zachlefin@gmail.com>
2020-01-01 18:03:59 -0500

    json special case

--- scanserver.py
+++ scanserver.py
@@ -1,9 +1,15 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import requests
+import json
 
 
-def temp_func():
-    return bytes('wewlad', 'utf-8')
+def get_table_data():
+    d = [
+        {"column1": "a", "column2": "b"},
+        {"column1": "c", "column2": "d"},
+        {"column1": "e", "column2": "f"},
+    ]
+    jsf = json.dumps(d)
+    return bytes(jsf, 'utf-8')
 
 
 class ScanHandler(BaseHTTPRequestHandler):
@@ -25,17 +31,23 @@ class ScanHandler(BaseHTTPRequestHandler):
         elif self.path.endswith('.js'):
             mimetype = 'application/javascript'
             sendReply = True
+        elif self.path.endswith('.json'):
+            mimetype = 'application/json'
+            sendReply = True
         elif self.path.endswith('.jpg'):
             mimetype = 'image/jpg'
             sendReply = True
 
         if sendReply:
             try:
-                with open('./webtable' + self.path, 'rb') as f:
-                    self.send_response(200)
-                    self.send_header('Content-type', mimetype)
-                    self.end_headers()
-                    self.wfile.write(f.read())
+                self.send_response(200)
+                self.send_header('content-type', mimetype)
+                self.end_headers()
+                if self.path == '/data.json':
+                    self.wfile.write(get_table_data())
+                else:
+                    with open('./webtable' + self.path, 'rb') as f:
+                        self.wfile.write(f.read())
 
             except IOError as e:
                 self.send_response(404)
