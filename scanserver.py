1689c7e1633810437106080fbfd20a3255707eed
Zach Lefin <zachlefin@gmail.com>
2020-01-03 13:55:02 -0500

    now takes scan function argument

--- scanserver.py
+++ scanserver.py
@@ -1,18 +1,10 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import json
-
-
-def get_table_data():
-    d = [
-        {"column1": "a", "column2": "b"},
-        {"column1": "c", "column2": "d"},
-        {"column1": "e", "column2": "f"},
-    ]
-    jsf = json.dumps(d)
-    return bytes(jsf, 'utf-8')
+import valuehunter as vh
+from colorama import Fore
 
 
 class ScanHandler(BaseHTTPRequestHandler):
+    scan_func = None
 
     def do_GET(self):
         mimetype = 'text/plain'  # default case
@@ -20,31 +12,31 @@ class ScanHandler(BaseHTTPRequestHandler):
         if self.path == '/':
             self.path = '/table.html'
 
-        sendReply = False
+        send_reply = False
 
         if self.path.endswith('.html'):
             mimetype = 'text/html'
-            sendReply = True
+            send_reply = True
         elif self.path.endswith('.css'):
             mimetype = 'text/css'
-            sendReply = True
+            send_reply = True
         elif self.path.endswith('.js'):
             mimetype = 'application/javascript'
-            sendReply = True
+            send_reply = True
         elif self.path.endswith('.json'):
             mimetype = 'application/json'
-            sendReply = True
+            send_reply = True
         elif self.path.endswith('.jpg'):
             mimetype = 'image/jpg'
-            sendReply = True
+            send_reply = True
 
-        if sendReply:
+        if send_reply:
             try:
                 self.send_response(200)
                 self.send_header('content-type', mimetype)
                 self.end_headers()
                 if self.path == '/data.json':
-                    self.wfile.write(get_table_data())
+                    self.wfile.write(self.get_table_data())
                 else:
                     with open('./webtable' + self.path, 'rb') as f:
                         self.wfile.write(f.read())
@@ -53,12 +45,23 @@ class ScanHandler(BaseHTTPRequestHandler):
                 self.send_response(404)
                 print(e)
 
+    def get_table_data(self):
+        df = ScanHandler.scan_func()
+        jsf = df.to_json(orient='records')
+        return bytes(jsf, 'utf-8')
 
-if __name__ == '__main__':
+
+def start_server(scan_function):
+    ScanHandler.scan_func = scan_function
     server = HTTPServer(('', 7531), ScanHandler)
     try:
-        print('Server start')
+        print(Fore.GREEN + 'Server start')
         server.serve_forever()
     except KeyboardInterrupt:
-        print('Server stop')
+        print(Fore.GREEN + 'Server stop')
         server.socket.close()
+
+
+if __name__ == '__main__':
+    data_func = lambda: vh.data.local.get_price_history('ROKU')
+    start_server(data_func)
